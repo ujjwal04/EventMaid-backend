e2b1a6a1e4582165338a69c7ff50f4a916154668
ujjwal <ujjawalverma77@gmail.com>
2021-05-25 15:16:03 +0530

    connected to atlas using mongoose

--- database.js
+++ database.js
@@ -1,18 +1,32 @@
-const sequelize = require('sequelize');
+// const sequelize = require('sequelize');
 
-const db = new sequelize({
-  dialect: 'mysql',
-  database: process.env.db,
-  host: process.env.host,
-  username: process.env.user,
-  password: process.env.password,
-  define: { charset: 'utf8mb4', dialectOptions: { collate: 'utf8mb4_bin' } },
-});
+// const db = new sequelize({
+//   dialect: 'mysql',
+//   database: process.env.db,
+//   host: process.env.host,
+//   username: process.env.user,
+//   password: process.env.password,
+//   define: { charset: 'utf8mb4', dialectOptions: { collate: 'utf8mb4_bin' } },
+// });
 
-db.sync()
-  .then(() => {
-    console.log('DB connection successful!!');
+// db.sync()
+//   .then(() => {
+//     console.log('DB connection successful!!');
+//   })
+//   .catch((err) => console.log(err));
+
+// module.exports = db;
+
+const mongoose = require('mongoose');
+
+const DB = process.env.DB.replace('<password>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .catch((err) => console.log(err));
-
-module.exports = db;
+  .then((con) => console.log('DB connection successful !!'))
+  .catch((err) => console.log('DB Connection error !!'));
